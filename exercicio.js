ce1098c9e694f0b8413b92d5b19e57f814960810
Kruguinn <162449721+Kruguinn@users.noreply.github.com>
2025-03-10 08:13:44 -0300

    Add files via upload

--- exercicio.js
+++ exercicio.js
@@ -3,3 +3,14 @@ let agua = 0.035
 
 let mult = peso * agua
 console.log("Lucas precisa beber " + mult + " litros por dia.")
+
+
+const av1= 10.0
+const av2= 7.0
+const av3= 4.0
+const media= (av1 + av2 + av3)/3
+if (media>=6.0){
+    console.log("Aprovado com nota " + media);
+} else {
+    console.log ("Reprovado");
+}
