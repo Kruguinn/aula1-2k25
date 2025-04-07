b963e673a71a82f2e28c1c4acbd6f38bbe4b3975
Kruguinn <162449721+Kruguinn@users.noreply.github.com>
2025-04-07 08:36:12 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,71 @@
+// OBJETOS //
+let times=[
+    {
+        nome: "Magnus Futsal"
+        goleiro: "Leandro Filho"
+        AláDireita: "Leandro Lino"
+        AláEsquerdo: "Dieguinho"
+        pivô: "Leozin"
+        fixo: "Rodrigo Capita"
+        técnico: "Ricardinho"
+        vitórias: 1
+        derrotas: 0
+    }
+];
+
+[
+    {
+        nome: "Seleção Brasileira"
+        goleiro: "Willian"
+        AláDireita: "Leandro Lino"
+        AláEsquerdo: "Arthur"
+        pivô: "Pito"
+        fixo: "Neguinho"
+        técnico: "Marquinhos Xavier"
+        vitórias: 7
+        derrotas: 0
+    }
+];
+
+[
+    {
+        nome: "Pato Futsal"
+        goleiro: "Djony"
+        AláDireita: "Ian"
+        AláEsquerdo: "Duduzinho"
+        pivô: "Ton"
+        fixo: "Gleidson"
+        técnico: "Betinho"
+        vitórias: 0
+        derrotas: 0
+    }
+];
+
+[
+    {
+        nome: "Marreco"
+        goleiro: "Juba"
+        AláDireita: "Canhoto"
+        AláEsquerdo: "Ligeiro"
+        pivô: "Fabinho"
+        fixo: "Nascimento"
+        técnico: "Banana"
+        vitórias: 0
+        derrotas: 0
+    }
+];
+
+[
+    {
+        nome: "Atlântico"
+        goleiro: "Ryn"
+        AláDireita: "PL"
+        AláEsquerdo: "Salah"
+        pivô: "Elenilson"
+        fixo: "Erick"
+        técnico: "Paulinho Sananduva"
+        vitórias: 0
+        derrotas: 0
+    }
+];
+
